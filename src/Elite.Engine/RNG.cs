1605c1b350260deed0888e5cde015c97615bdb23
Andy Hawkins <aph828@gmail.com>
2023-05-07 14:44:01 +0100

    tidy rng

--- src/Elite.Engine/RNG.cs
+++ src/Elite.Engine/RNG.cs
@@ -1,28 +1,12 @@
-/*
- * Elite - The New Kind.
- *
- * Reverse engineered from the BBC disk version of Elite.
- * Additional material by C.J.Pinder.
- *
- * The original Elite code is (C) I.Bell & D.Braben 1984.
- * This version re-engineered in C by C.J.Pinder 1999-2001.
- *
- * email: <christian@newkind.co.uk>
- *
- *
- */
+// 'Elite - The Sharp Kind' - Andy Hawkins 2023.
+// 'Elite - The New Kind' - C.J.Pinder 1999-2001.
+// Elite (C) I.Bell & D.Braben 1984.
 
 namespace Elite.Engine
 {
     internal static class RNG
     {
-        internal static RandomSeed Seed = new();
-
-        internal static int Random(int maxValue) => Random(0, maxValue);
-
-        internal static int Random(int minValue, int maxValue) => new Random().Next(minValue, maxValue + 1);
-
-        internal static bool TrueOrFalse() => Random(0, 1) == 1;
+        internal static RandomSeed Seed { get; set; } = new();
 
         /// <summary>
         /// Guassian random number generator.
@@ -42,6 +26,31 @@ namespace Elite.Engine
             r /= iterations;
 
             return r;
+        }
+
+        /// <summary>
+        /// Generate a random number between 0 and 255.
+        /// This is the version used in the 6502 Elites.
+        /// </summary>
+        /// <returns>A random number between 0 and 255.</returns>
+        internal static int GenerateRandomNumber()
+        {
+            int x = (Seed.A * 2) & 0xFF;
+            int a = x + Seed.C;
+            if (Seed.A > 127)
+            {
+                a++;
+            }
+
+            Seed.A = a & 0xFF;
+            Seed.C = x;
+
+            a /= 256;    /* a = any carry left from above */
+            x = Seed.B;
+            a = (a + x + Seed.D) & 0xFF;
+            Seed.B = a;
+            Seed.D = x;
+            return a;
         }
 
         /// <summary>
@@ -71,29 +80,10 @@ namespace Elite.Engine
             return Seed.C / 0x34;
         }
 
-        /// <summary>
-        /// Generate a random number between 0 and 255.
-        /// This is the version used in the 6502 Elites.
-        /// </summary>
-        /// <returns>A random number between 0 and 255.</returns>
-        internal static int GenerateRandomNumber()
-        {
-            int x = (Seed.A * 2) & 0xFF;
-            int a = x + Seed.C;
-            if (Seed.A > 127)
-            {
-                a++;
-            }
+        internal static int Random(int maxValue) => Random(0, maxValue);
 
-            Seed.A = a & 0xFF;
-            Seed.C = x;
+        internal static int Random(int minValue, int maxValue) => new Random().Next(minValue, maxValue + 1);
 
-            a /= 256;    /* a = any carry left from above */
-            x = Seed.B;
-            a = (a + x + Seed.D) & 0xFF;
-            Seed.B = a;
-            Seed.D = x;
-            return a;
-        }
+        internal static bool TrueOrFalse() => Random(0, 1) == 1;
     }
 }
