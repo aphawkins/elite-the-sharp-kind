77b2af1aa76399d4cf6958f660a28cd04f12ca6d
aphawkins <aph828@gmail.com>
2023-05-10 21:55:23 +0100

    fix IDE1006

--- src/Elite.Engine/Views/ThargoidMission.cs
+++ src/Elite.Engine/Views/ThargoidMission.cs
@@ -10,26 +10,26 @@ namespace Elite.Engine.Views
 {
     internal sealed class ThargoidMissionView : IView
     {
-        private const string s_mission2BriefA =
+        private const string Mission2BriefA =
             "Attention Commander, I am Captain Fortesque of Her Majesty's Space Navy. " +
             "We have need of your services again. If you would be so good as to go to " +
             "Ceerdi you will be briefed.If succesful, you will be rewarded." +
             "---MESSAGE ENDS.";
 
-        private const string s_mission2BriefB =
+        private const string Mission2BriefB =
             "Good Day Commander. I am Agent Blake of Naval Intelligence. As you know, " +
             "the Navy have been keeping the Thargoids off your ass out in deep space " +
             "for many years now. Well the situation has changed. Our boys are ready " +
             "for a push right to the home system of those murderers.";
 
-        private const string s_mission2BriefC =
+        private const string Mission2BriefC =
             "I have obtained the defence plans for their Hive Worlds. The beetles " +
             "know we've got something but not what. If I transmit the plans to our " +
             "base on Birera they'll intercept the transmission. I need a ship to " +
             "make the run. You're elected. The plans are unipulse coded within " +
             "this transmission. You will be paid. Good luck Commander. ---MESSAGE ENDS.";
 
-        private const string s_mission2Debrief =
+        private const string Mission2Debrief =
             "You have served us well and we shall remember. " +
             "We did not expect the Thargoids to find out about you." +
             "For the moment please accept this Navy Extra Energy Unit as payment. " +
@@ -55,14 +55,14 @@ namespace Elite.Engine.Views
             if (_gameState.Cmdr.Mission == 4)
             {
                 _draw.DrawViewHeader("INCOMING MESSAGE");
-                _draw.DrawTextPretty(116, 132, 400, s_mission2BriefA);
+                _draw.DrawTextPretty(116, 132, 400, Mission2BriefA);
                 _gfx.DrawTextCentre(330, "Press space to continue.", 140, Colour.Gold);
             }
             else if (_gameState.Cmdr.Mission == 5)
             {
                 _draw.DrawViewHeader("INCOMING MESSAGE");
-                _draw.DrawTextPretty(16, 50, 300, s_mission2BriefB);
-                _draw.DrawTextPretty(16, 200, 470, s_mission2BriefC);
+                _draw.DrawTextPretty(16, 50, 300, Mission2BriefB);
+                _draw.DrawTextPretty(16, 200, 470, Mission2BriefC);
                 _gfx.DrawImage(Image.Blake, new(352, 46));
                 _gfx.DrawTextCentre(330, "Press space to continue.", 140, Colour.Gold);
             }
@@ -70,7 +70,7 @@ namespace Elite.Engine.Views
             {
                 _draw.DrawViewHeader("INCOMING MESSAGE");
                 _gfx.DrawTextCentre(100, "Well done Commander.", 140, Colour.Gold);
-                _draw.DrawTextPretty(116, 132, 400, s_mission2Debrief);
+                _draw.DrawTextPretty(116, 132, 400, Mission2Debrief);
                 _gfx.DrawTextCentre(330, "Press space to continue.", 140, Colour.Gold);
             }
         }
