11d0c8d9218ae83d1589d3fd66b0b81c66baebf4
Andy Hawkins <aph828@gmail.com>
2023-06-29 18:37:01 +0100

    fix market position

--- src/EliteSharp/Views/MarketView.cs
+++ src/EliteSharp/Views/MarketView.cs
@@ -32,11 +32,11 @@ namespace EliteSharp.Views
         {
             _draw.DrawViewHeader($"{_planet.NamePlanet(_gameState.DockedPlanet)} MARKET PRICES");
 
-            _graphics.DrawTextLeft(new(16, 40), "PRODUCT", Colour.Green);
-            _graphics.DrawTextLeft(new(166, 40), "UNIT", Colour.Green);
-            _graphics.DrawTextLeft(new(246, 40), "PRICE", Colour.Green);
-            _graphics.DrawTextLeft(new(314, 40), "FOR SALE", Colour.Green);
-            _graphics.DrawTextLeft(new(420, 40), "IN HOLD", Colour.Green);
+            _graphics.DrawTextLeft(new(16 + _draw.Offset, 40), "PRODUCT", Colour.Green);
+            _graphics.DrawTextLeft(new(166 + _draw.Offset, 40), "UNIT", Colour.Green);
+            _graphics.DrawTextLeft(new(246 + _draw.Offset, 40), "PRICE", Colour.Green);
+            _graphics.DrawTextLeft(new(314 + _draw.Offset, 40), "FOR SALE", Colour.Green);
+            _graphics.DrawTextLeft(new(420 + _draw.Offset, 40), "IN HOLD", Colour.Green);
 
             int i = 0;
             foreach (KeyValuePair<StockType, StockItem> stock in _trade.StockMarket)
@@ -45,26 +45,26 @@ namespace EliteSharp.Views
 
                 if (stock.Key == _highlightedStock)
                 {
-                    _graphics.DrawRectangleFilled(new(2, y), 508, 15, Colour.LightRed);
+                    _graphics.DrawRectangleFilled(new(2 + _draw.Offset, y), 508, 15, Colour.LightRed);
                 }
 
-                _graphics.DrawTextLeft(new(16, y), stock.Value.Name, Colour.White);
+                _graphics.DrawTextLeft(new(16 + _draw.Offset, y), stock.Value.Name, Colour.White);
 
-                _graphics.DrawTextLeft(new(180, y), stock.Value.Units, Colour.White);
+                _graphics.DrawTextLeft(new(180 + _draw.Offset, y), stock.Value.Units, Colour.White);
 
-                _graphics.DrawTextRight(285, y, $"{stock.Value.CurrentPrice:N1}", Colour.White);
+                _graphics.DrawTextRight(285 + _draw.Offset, y, $"{stock.Value.CurrentPrice:N1}", Colour.White);
 
-                _graphics.DrawTextRight(365, y, stock.Value.CurrentQuantity > 0 ? $"{stock.Value.CurrentQuantity}" : "-", Colour.White);
-                _graphics.DrawTextLeft(new(365, y), stock.Value.CurrentQuantity > 0 ? stock.Value.Units : string.Empty, Colour.White);
+                _graphics.DrawTextRight(365 + _draw.Offset, y, stock.Value.CurrentQuantity > 0 ? $"{stock.Value.CurrentQuantity}" : "-", Colour.White);
+                _graphics.DrawTextLeft(new(365 + _draw.Offset, y), stock.Value.CurrentQuantity > 0 ? stock.Value.Units : string.Empty, Colour.White);
 
-                _graphics.DrawTextRight(455, y, stock.Value.CurrentCargo > 0 ? $"{stock.Value.CurrentCargo,2}" : "-", Colour.White);
-                _graphics.DrawTextLeft(new(455, y), stock.Value.CurrentCargo > 0 ? stock.Value.Units : string.Empty, Colour.White);
+                _graphics.DrawTextRight(455 + _draw.Offset, y, stock.Value.CurrentCargo > 0 ? $"{stock.Value.CurrentCargo,2}" : "-", Colour.White);
+                _graphics.DrawTextLeft(new(455 + _draw.Offset, y), stock.Value.CurrentCargo > 0 ? stock.Value.Units : string.Empty, Colour.White);
 
                 i++;
             }
 
-            _graphics.DrawTextLeft(new(16, 340), "Cash:", Colour.Green);
-            _graphics.DrawTextRight(160, 340, $"{_trade.Credits,10:N1} Credits", Colour.White);
+            _graphics.DrawTextLeft(new(16 + _draw.Offset, 340), "Cash:", Colour.Green);
+            _graphics.DrawTextRight(160 + _draw.Offset, 340, $"{_trade.Credits,10:N1} Credits", Colour.White);
         }
 
         public void HandleInput()
