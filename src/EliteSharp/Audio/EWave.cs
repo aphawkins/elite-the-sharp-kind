4852a1a2505dabc4ceb4200a7fc632e3f21a833e
Andy Hawkins <aph828@gmail.com>
2023-11-16 20:30:44 +0000

    fix IDE0290

--- src/EliteSharp/Audio/EWave.cs
+++ src/EliteSharp/Audio/EWave.cs
@@ -1,13 +1,11 @@
-﻿// 'Elite - The Sharp Kind' - Andy Hawkins 2023.
+// 'Elite - The Sharp Kind' - Andy Hawkins 2023.
 // 'Elite - The New Kind' - C.J.Pinder 1999-2001.
 // Elite (C) I.Bell & D.Braben 1984.
 
 namespace EliteSharp.Audio
 {
-    internal class EWave
+    internal class EWave(byte[] bytes)
     {
-        public EWave(byte[] bytes) => Bytes = bytes;
-
-        public byte[] Bytes { get; }
+        public byte[] Bytes { get; } = bytes;
     }
 }
